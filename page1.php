10ae0ced011f301a98443810f63c81428b78a97b
rimurun <rimuru930@gmail.com>
2020-05-31 00:15:28 +0900

    add first by rikuta

--- page1.php
+++ page1.php
@@ -8,6 +8,8 @@ setcookie('value',100);
     $getcookie=$_COOKIE['value'];
     print"このページ1のクッキーの値は「".$getcookie."」です<br/>";
 ?>
-        <a href ="page2.php">ページ２へ</a>
+       <p>なるほどね。なるほどね。なるほどね。「めんどくせぇな、3回被せて突っ込ませる件やろうとすんなよ</p>
+       <br>       
+       <a href ="page2.php">ページ２へ</a>
     </body>
 </html>
